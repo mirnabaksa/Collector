52f7161e48b404ff32c157b41d797d98d6c8ca84
Mirna Baksa <mirna.baksa@fer.hr>
2018-06-19 20:08:14 +0200

    Changed db config

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => RDS_HOSTNAME,
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => RDS_DB_NAME,
-            'username' => RDS_USERNAME,
-            'password' => RDS_PASSWORD,
+            'database' => env('DB_DATABASE', 'collector'),
+            'username' => env('DB_USERNAME', 'collector'),
+            'password' => env('DB_PASSWORD', 'co11ecTOR%'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
